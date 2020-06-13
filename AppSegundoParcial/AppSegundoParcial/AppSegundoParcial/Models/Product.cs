24e5d48139e0cf41f19936f3554c784393eab5ed
Jhon Alexander Romero Arancibia <a2017210251@estudiantes.upsa.edu.bo>
2020-06-13 10:46:42 -0400

    .

---
+++ AppSegundoParcial/AppSegundoParcial/AppSegundoParcial/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace AppSegundoParcial.Models
+{
+    
+   
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class Temperatures
+    {
+        [JsonProperty("albumId")]
+        public long AlbumId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public long Title { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("thumbnailUrl")]
+        public Uri ThumbnailUrl { get; set; }
+    }
+}
+
+
